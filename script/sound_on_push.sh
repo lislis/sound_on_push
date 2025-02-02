5cc067afdae80e21c24a7fd21b78b0d2803c5320
lislis <lislis@users.noreply.github.com>
2025-02-02 16:48:49 +0100

    Update sound_on_push.sh

--- script/sound_on_push.sh
+++ script/sound_on_push.sh
@@ -2,4 +2,4 @@
 
 # call the binary with the path to the soundfile
 # adjust paths as necessary
-/home/raspi/sound_on_push/target/debug/sound_on_push /home/raspi/soundfile.wav
+/home/raspi/sound_on_push/target/debug/sound_on_push /home/raspi/soundfile.wav -4
